29f3d01c471a09357eb2df68bd249ec8bbb979d7
unknown <giovannidelvecchio4@gmail.com>
2020-04-03 17:21:17 +0200

    aggiunti alcuni files

---
+++ ContinuousAttribute.java
@@ -0,0 +1,9 @@
+
+public class ContinuousAttribute extends Attribute
+{
+	//Metodi
+	ContinuousAttribute(String name, int index)
+	{
+		super(name, index);
+	}
+}
---
+++ DiscreteAttribute.java
@@ -0,0 +1,30 @@
+
+public class DiscreteAttribute extends Attribute 
+{
+	//Attributi
+	private String values[];
+	
+	//Metodi
+	DiscreteAttribute(String name, int index, String values[])
+	{
+		super(name, index);
+		this.values = values;
+	}
+	
+	int getNumberOfDistinctValues() 
+	{
+		return values.length;
+	}
+	
+	String getValue(int i) 
+	{
+		if(i < getNumberOfDistinctValues()) 
+		{
+			return values[i];
+		}
+		return "out of bound";	//da sostituire con la 
+								//giusta eccezione
+	}
+		
+}
+
