52ca4654994e1327a5b0996f115307ab03e82745
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-03 18:08:41 +0200

    sistemate parentesi

--- src/Attribute.java
+++ src/Attribute.java
@@ -4,27 +4,24 @@ public abstract class Attribute
 {
 	//Attributi
 	private String name; //nome simbolico dell'attributo
+	
 	private int index; 	//identificativo numerico dell'attributo
 	
 	//Metodi
-	Attribute(String name, int index)
-	{
+	Attribute(String name, int index) {
 		this.name = name;
 		this.index = index;
 	}
 	
-	String getName() 
-	{
+	String getName() {
 		return name;
 	}
 	
-	int getIndex() 
-	{
+	int getIndex() {
 		return index;
 	}
 	
-	public String toString() 
-	{
+	public String toString() {
 		return getName() + " " + getIndex();
 	}
 }
