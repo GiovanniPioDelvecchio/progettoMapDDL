7b7dd0bbcd9b081a199500520cd1b2eb739fdbb2
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-03 18:43:21 +0200

    completata sistemazione stile

--- src/Attribute.java
+++ src/Attribute.java
@@ -9,20 +9,26 @@ public abstract class Attribute
 	
 	//Metodi
 	Attribute(String name, int index) {
+		
 		this.name = name;
 		this.index = index;
 	}
-	
+		
 	String getName() {
+	
 		return name;
 	}
 	
 	int getIndex() {
+	
 		return index;
+	
 	}
 	
 	public String toString() {
+	
 		return getName() + " " + getIndex();
 	}
+	
 }
 
--- src/DiscreteAttribute.java
+++ src/DiscreteAttribute.java
@@ -7,16 +7,19 @@ public class DiscreteAttribute extends Attribute
 	
 	//Metodi
 	DiscreteAttribute(String name, int index, String values[]) {
+	
 		super(name, index);
 		this.values = values;
 	}
 	
 	int getNumberOfDistinctValues() {
+	
 		return values.length;
 	}
 	
 	String getValue(int i) {
-		if(i < getNumberOfDistinctValues()) {
+		
+		if (i < getNumberOfDistinctValues()) {		
 			return values[i];
 		}
 		return "out of bound";	//da sostituire con la 
