1d383d929735effbf7feb361496e1a32202dabf2
DomMcOyle <zeito99@gmail.com>
2020-07-27 16:37:27 +0200

    Commit breve per pullare

--- map7Server/src/data/Attribute.java
+++ map7Server/src/data/Attribute.java
@@ -2,14 +2,34 @@ package data;
 
 import java.io.Serializable;
 
+/**
+ * Classe astratta creata per modellare un generico attributo di un esempio.<br>
+ * Essa tiene traccia del nome dell'attributo e dell'indice intero rappresentante la "colonna"
+ * che nel dataset raccoglie i valori di tale attributo e che conseguentemente lo identifica.<br>
+ * Una sottoclasse che eredita da Attribute non ha metodi astratti da implementare.<br>
+ * 
+ * @author Domenico Dell'Olio, Giovanni Pio Delvecchio, Giuseppe Lamantea
+ *
+ */
+
 public abstract class Attribute implements Serializable {
 	
-	// Attributi
-	private String name;	// nome simbolico dell'attributo
+	/**
+	 * nome simbolico dell'attributo
+	 */
+	private String name;
+
+	/**
+	 * identificativo numerico dell'attributo
+	 */
+	private int index;
 	
-	private int index; 		// identificativo numerico dell'attributo
-	
-	// Metodi
+	/**
+	 * Costruttore di Attribute
+	 * Inizializza il nome e l'indice dell'attributo con i parametri passati in input.
+	 * @param name
+	 * @param index
+	 */
 	public Attribute(String name, int index) {
 		
 		this.name = name;
