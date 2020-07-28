8c20bc78c3e824faad6e6b80e87223d1c11ce3f7
DomMcOyle <zeito99@gmail.com>
2020-07-28 17:44:53 +0200

    sistemata classe DatabaseConnectionException

--- map7Server/src/database/DatabaseConnectionException.java
+++ map7Server/src/database/DatabaseConnectionException.java
@@ -1,22 +1,48 @@
 package database;
 
+/**
+ * Classe che modella un'eccezione generata durante la connessione al Database.
+ * Poichè può essere lanciata solamente all'interno del package <code>database</code>
+ * ma gestita all'esterno, è pubblica mentre i suoi costruttori hanno visibilità package.
+ * 
+ * @author Domenico Dell'Olio, Giovanni Pio Delvecchio, Giuseppe Lamantea
+ *
+ */
+
+@SuppressWarnings("serial")
 public class DatabaseConnectionException extends Exception {
 
-	public String toAdd = ": Failed to connect to the database";
-	
-	public DatabaseConnectionException() {
+	/**
+	 * Costruttore a 0 args di DbCException.
+	 * Richiama il costruttore della classe Exception.
+	 */
+	DatabaseConnectionException() {
 		
 		super();
 	}
 	
-	public DatabaseConnectionException(String toAdd) {
+	/**
+	 * Costruttore con messaggio di DbException.
+	 * Richiama il costruttore con messaggio della classe Exception.
+	 * 
+	 * @param message stringa contenente la descrizione dell'errore che l'eccezione modella.
+	 */
+	DatabaseConnectionException(String message) {
 		
-		super();
-		this.toAdd = toAdd;
+		super(message);
 	}
 	
+	/**
+	 * Sovrascrittura del metodo <code>toString()</code> di <code>Object</code>
+	 * 
+	 * @return una stringa contenente il nome dell'eccezione concatenato al messaggio, se c'è, o
+	 * 		   ad un messaggio generico.
+	 */
 	public String toString() {
 		
-		return this.getClass().getName() + toAdd;
+		if (this.getMessage() == null )
+			return this.getClass().getName() + ": Failure during connection.";
+		 else
+			return this.getClass().getName() + ": " + this.getMessage();
 	}
 }
