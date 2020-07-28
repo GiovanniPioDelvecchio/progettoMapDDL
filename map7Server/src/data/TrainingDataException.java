fe0f6af7c5fc9cc413910298d304ae611304e30d
DomMcOyle <zeito99@gmail.com>
2020-07-28 16:19:52 +0200

    Aggiunti i commenti alla classe TDException

--- map7Server/src/data/TrainingDataException.java
+++ map7Server/src/data/TrainingDataException.java
@@ -1,14 +1,32 @@
 package data;
 
+/**
+ * Classe che modella un'eccezione specificatamente lanciata durante la costruzione di un training set.<br>
+ * Poichè è possibile lanciarla solamente all'interno del costruttore di Data ma è possibile gestirla in
+ * un altro punto del programma, la classe è pubblica mentre il costruttore ha visibilità package.
+ *
+ * @author Domenico Dell'Olio, Giovanni Pio Delvecchio, Giuseppe Lamantea
+ *
+ */
 public class TrainingDataException extends Exception {
 
-	
-
+	/**
+	 * Costruttore della Eccezione.
+	 * Poichè è possibile lanciarla solo durante la costruzione di un'istanza di Data, la visibiltà
+	 * è limitata al package. Chiama il costruttore di Exception.
+	 * 
+	 * @param message Stringa contente un messaggio descrittivo della situazione d'eccezione
+	 */
 	TrainingDataException(String message) {
 
 		super(message);
 	}
 
+	/**
+	 * Sovrascrittura del metodo toString di object
+	 * 
+	 * @return una stringa che racchiude il nome e il messaggio dell'eccezione.
+	 */
 	public String toString() {
 		
 		return this.getClass().getName() +": "+ this.getMessage();
