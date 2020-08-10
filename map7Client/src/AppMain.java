f9d9e23d19ca6f8eb5058d1ede130e49ce5efd19
DomMcOyle <zeito99@gmail.com>
2020-08-10 17:17:04 +0200

    Aggiunta bozza main

--- map7Client/src/AppMain.java
+++ map7Client/src/AppMain.java
@@ -1,5 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-
-public class AppMain {
+public class AppMain extends Application {
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+	public void start(Stage mainStage) {
+		
+		
+	}
 
 }
