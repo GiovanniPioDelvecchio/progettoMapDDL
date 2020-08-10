c092096f1666c020f78acac408eadb82edc5c843
DomMcOyle <zeito99@gmail.com>
2020-08-10 17:35:40 +0200

    Aggiunti attributi main

--- map7Client/src/AppMain.java
+++ map7Client/src/AppMain.java
@@ -1,7 +1,19 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
+	
+	private Socket clientSocket;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	private String ip = "localhost";
+	private int PORT = 8080;
+	
 	
 	public static void main(String[] args) {
 		
