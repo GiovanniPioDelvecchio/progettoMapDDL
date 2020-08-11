9da4007cede2ba7c7f9f0a872e4b29f93c119708
unknown <giovannidelvecchio4@gmail.com>
2020-08-11 17:08:01 +0200

    Aggiunte le parti di connessione e invio dei primi due messaggi (0 e 2 per caricamento e creazione)

--- map7Client/src/AppMain.java
+++ map7Client/src/AppMain.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -6,6 +7,7 @@ import java.net.Socket;
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
@@ -23,8 +25,8 @@ public class AppMain extends Application {
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
 	private String ip = "localhost";
-	private int PORT = 8080;
-	
+	private int port = 8080;
+	private String dataName = "servo";
 	
 	public static void main(String[] args) {
 		
@@ -70,12 +72,99 @@ public class AppMain extends Application {
 		pane.setCenter(centralPanel);
 		
 		/*
+		 * Se il tasto "Carica" viene premuto, ci si connette al server e si comunica di caricare
+		 * dal file l'albero corrispondente al nome del file scelto
+		 */
+		
+		load.setOnAction(e->{
+			
+			tryConnection();
+			loadFromFile();
+		
+		});
+		/*
+		 * Se il tasto "Crea" viene premuto, ci si connette al server e si comunica di caricare
+		 * da un database la tabella da cui verrà ricavato l'albero di regressione
+		 */
+		create.setOnAction(e->{
+				
+			tryConnection();
+			createFromDB();
+		});
+		
+		/*
 		 * chiamata ai metodi per mostrare la scena principale
 		 */
+		
 		
 		Scene homeScene = new Scene(pane, 400, 400);
 		mainStage.setScene(homeScene);
 		mainStage.show();
 	}
 
+	
+	void tryConnection() {
+		
+		try {
+			clientSocket = new Socket(ip, port);
+			System.out.println(clientSocket);		
+			out = new ObjectOutputStream(clientSocket.getOutputStream());
+			in = new ObjectInputStream(clientSocket.getInputStream());
+			
+		} catch(IOException e) {
+			
+			/*
+			 * Se qualcosa va storto con la creazione del collegamento si mostra un alert
+			 */
+			showAlert("Non è stato possibile connettersi al server selezionato");
+		}
+	}
+	
+	void loadFromFile() {
+		
+		try {
+			
+			out.writeObject(2);
+			out.writeObject(dataName);
+			
+		} catch(IOException e) {
+			
+			/*
+			 * Se qualcosa va storto con l'invio dei messaggi al server si mostra un alert
+			 */
+
+			showAlert("Non è stato possibile comunicare l'operazione al server selezionato");
+		}	
+		
+	}
+	
+	void createFromDB() {
+		
+		try {
+			
+			out.writeObject(0);
+			out.writeObject(dataName);
+			
+		} catch(IOException e) {
+			
+			/*
+			 * Se qualcosa va storto con l'invio dei messaggi al server si mostra un alert
+			 */
+			
+			showAlert("Non è stato possibile comunicare l'operazione al server selezionato");
+		}
+		
+	}
+	
+	void showAlert(String message) {
+		
+		
+		/*
+		 * Se qualcosa va storto è possibile mostrare una finestra di dialogo con un messaggio
+		 */
+		
+		Alert toShow = new Alert(Alert.AlertType.ERROR);
+		toShow.setContentText(message);
+		toShow.show();
+	}
 }
