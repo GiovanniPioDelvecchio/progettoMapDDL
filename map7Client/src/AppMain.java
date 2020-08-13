ba70f3406c77ccfea8c4333872658483f675f3b7
unknown <giovannidelvecchio4@gmail.com>
2020-08-13 19:06:53 +0200

    Aggiunta la predict class come scena (da migliorare)

--- map7Client/src/AppMain.java
+++ map7Client/src/AppMain.java
@@ -11,7 +11,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -34,7 +36,9 @@ public class AppMain extends Application {
 	private String ip = "127.0.0.1";
 	private int port = 8080;
 
-	private Scene selectionScene, homeScene, settingsScene;
+	private Scene selectionScene, homeScene, settingsScene, predictScene;
+	
+	private boolean loadFlag = false;
 
 	public static void main(String[] args) {
 		
@@ -80,6 +84,7 @@ public class AppMain extends Application {
 		centralPanel.getChildren().addAll(sel, load,create);
 		homePane.setCenter(centralPanel);
 		
+		
 		/*
 		 * Se il tasto "Carica" viene premuto, ci si connette al server e si comunica di caricare
 		 * dal file l'albero corrispondente al nome del file scelto
@@ -88,7 +93,7 @@ public class AppMain extends Application {
 		load.setOnAction(e -> {
 			
 			try {
-
+				loadFlag = true;
 				connectToServer();
 				out.writeObject(2);
 				mainStage.setScene(selectionScene);
@@ -109,7 +114,7 @@ public class AppMain extends Application {
 		create.setOnAction(e -> {
 
 			try {
-
+				loadFlag = false;
 				connectToServer();
 				out.writeObject(0);
 				mainStage.setScene(selectionScene);
@@ -121,7 +126,96 @@ public class AppMain extends Application {
 				showAlert("Non e' stato possibile comunicare l'operazione al server selezionato");
 			}
 		});
-
+		
+		/** FINESTRA DI PREDIZIONE **/
+		
+		/*	
+		 * Si trova in questo punto perchÃ© la descrizione della scena di predizione
+		 *	deve precedere la predizione stessa (handlePredict)
+		 */
+		BorderPane predictPane = new BorderPane();
+		VBox predictBox = new VBox();
+		Label predictMessage = new Label();
+		Label predictedValue = new Label();
+		TextField userChoice = new TextField();
+		HBox predictButtons = new HBox();
+		Button confirmChoice = new Button("Conferma");
+		
+		
+		Button redo = new Button("Ricomincia");
+		redo.setDisable(true);
+		Button backPredict = new Button("Indietro");
+		backPredict.setOnAction(e -> { 
+			
+			if (clientSocket != null) {
+				if (clientSocket.isConnected()) {
+					try {
+	
+						// se ï¿½ in corso una comunicazione col server, si notifica che si sta tornando alla home
+						out.writeObject(-1);
+						clientSocket.close();
+					} catch (IOException e1) {
+	
+						showAlert("Errore durante la connessione al server");
+					}
+				}
+				
+			}
+			mainStage.setScene(homeScene);
+		});
+		
+		redo.setOnAction(e->{
+			
+			try {
+				out.writeObject(3);
+				predictedValue.setText("");
+				redo.setDisable(true);
+				handlePredict(predictMessage, predictedValue, redo);
+			} catch (IOException e1) {
+				
+				showAlert("Non Ã¨ stato possibile raggiungere il server");
+			}
+			
+		});
+		
+		confirmChoice.setOnAction(e->{
+			
+			if (!predictMessage.getText().equals("")) {
+				
+				
+				try {
+					
+					Integer toSend = Integer.parseInt(userChoice.getText());
+					userChoice.clear();
+					//System.out.println(toSend);
+					out.writeObject(toSend);
+					
+					handlePredict(predictMessage, predictedValue, redo);
+					
+				} catch (IOException e1) {
+					
+					showAlert("Errore nella comunicazione con il server");
+					
+				} catch (NumberFormatException e2) {
+					
+					showAlert("Inserire un numero intero");
+				}
+			}
+		});
+		
+		
+		
+		
+		predictButtons.getChildren().add(confirmChoice);
+		predictButtons.getChildren().add(redo);
+		predictButtons.getChildren().add(backPredict);
+		predictBox.getChildren().add(predictMessage);
+		predictBox.getChildren().add(userChoice);
+		predictBox.getChildren().add(predictButtons);
+		predictBox.getChildren().add(predictedValue);
+		predictPane.setCenter(predictBox);
+		
+		
 		/** INSERIMENTO TABELLA **/
 
 		// Si viene a creare un layout a griglia
@@ -141,6 +235,8 @@ public class AppMain extends Application {
 		
 		// Si inserisce un bottone di conferma che rimane disattivato se non viene inserito del testo
 		Button confirm = new Button("Conferma");
+		
+		
 		confirm.setDisable(true);
 		confirm.setOnAction(e -> {
 
@@ -148,6 +244,7 @@ public class AppMain extends Application {
 				
 				// Alla pressione si tenta di mandare il nome della tabella al server
 				out.writeObject(tableName.getText());
+
 				String ans = (String) in.readObject();
 				
 				if (!ans.equals("OK")) {
@@ -155,12 +252,33 @@ public class AppMain extends Application {
 					showAlert(ans);
 				} else {
 					
-					// mainStage.setScene(predictScene);
+					
+					
+					if (loadFlag == false) {
+						
+						out.writeObject(1);
+						if (!((String) in.readObject()).equals("OK")) {
+						
+							showAlert("Errore nel salvataggio dei dati");
+						}
+					}
+					
+					
+					
+					mainStage.setScene(predictScene);
+					
+					out.writeObject(3);
+					//confirmChoice.setDisable(false);
+					
+					handlePredict(predictMessage, predictedValue, redo);
+					
+					//confirmChoice.setDisable(true);
+
 				}
 			} catch (ClassNotFoundException | IOException e1) {
 				
 				/* 
-				 * si presuppone che la connessione sia già stata stabilita
+				 * si presuppone che la connessione sia giï¿½ stata stabilita
 				 * e pertanto non si prevede di gestire una NullPointerException
 				 */
 				showAlert("Errore durante la connessione al server");
@@ -169,26 +287,10 @@ public class AppMain extends Application {
 		selectionPane.add(confirm, 0, 2);
 		
 		// si crea anche un bottone per tornare indietro alla home
-		Button back = new Button("Indietro");
-		back.setOnAction(e -> { 
-			
-			if (clientSocket != null) {
-				if (clientSocket.isConnected()) {
-					try {
-	
-						// se è in corso una comunicazione col server, si notifica che si sta tornando alla home
-						out.writeObject("ABORT");
-						clientSocket.close();
-					} catch (IOException e1) {
-	
-						showAlert("Errore durante la connessione al server");
-					}
-				}
-				
-			}
-			mainStage.setScene(homeScene);
-		});
-		selectionPane.add(back, 1, 2);
+		Button backSelection= new Button("Indietro");
+		backSelection.setOnAction(backPredict.getOnAction());
+		
+		selectionPane.add(backSelection, 1, 2);
 		
 		// Si imposta il campo testuale in maniera che se risulta essere vuoto, il tasto di conferma viene disabilitato
 		tableName.setOnKeyReleased(e -> {
@@ -207,6 +309,10 @@ public class AppMain extends Application {
 		});
 		
 		
+		
+		
+		
+		
 		/** FINESTRA DELLE IMPOSTAZIONI **/
 		
 		GridPane settingsPane = new GridPane();
@@ -217,6 +323,10 @@ public class AppMain extends Application {
 		
 		Label ipLabel = new Label("Indirizzo IP");
 		Label portLabel = new Label("Porta");
+		
+		Button backSetting = new Button("Indietro");
+		backSetting.setOnAction(e->mainStage.setScene(homeScene));
+		
 		
 		/*
 		 * Per limitare il margine di errore nell'inserimento dell'indirizzo IP,
@@ -268,13 +378,13 @@ public class AppMain extends Application {
 		Button confirmButtonSettings = new Button("Conferma");
 		HBox backLayoutSettings = new HBox();
 		backLayoutSettings.setAlignment(Pos.CENTER_LEFT);
-		backLayoutSettings.getChildren().add(back);
+		backLayoutSettings.getChildren().add(backSetting);
 
 		/*
 		 * Si dichiara un oggetto di tipo EventHandler<ActionEvent>, il cui metodo handle descrive il comportamento
 		 * di conferma alla pressione del pulsante confirmButtonSettings (o alla pressione del tasto enter).
-		 * Si e' scelta un oggetto di classe anonima al posto di una lambda-espressione poichè il corpo
-		 * della funzione handle è molto esteso ed è pertanto più facilmente leggibile
+		 * Si e' scelta un oggetto di classe anonima al posto di una lambda-espressione poichï¿½ il corpo
+		 * della funzione handle ï¿½ molto esteso ed ï¿½ pertanto piï¿½ facilmente leggibile
 		 */
 		EventHandler<ActionEvent> confirmEvent = new EventHandler<ActionEvent>() {
 
@@ -326,7 +436,7 @@ public class AppMain extends Application {
 						ip = new String(newIp);
 					} else {
 		
-						showAlert("L'indirizzo IP inserito non è valido.\n"
+						showAlert("L'indirizzo IP inserito non ï¿½ valido.\n"
 								+ "I valori che compongono l'indirizzo devono essere interi da 0 a 255.");
 					}
 				}
@@ -340,7 +450,7 @@ public class AppMain extends Application {
 				
 				if (!readPort.equals("")) {
 				
-					final String errorMessage = "Il numero di porta inserito non è valido.\n"
+					final String errorMessage = "Il numero di porta inserito non ï¿½ valido.\n"
 							+ "Il numero di porta deve essere un intero fra 1 e 65535.";
 					int intPort;
 					try {
@@ -402,7 +512,8 @@ public class AppMain extends Application {
 		 */
 		homeScene = new Scene(homePane, 400, 400);
 		selectionScene = new Scene(selectionPane, 400, 400);
-		settingsScene = new Scene(settingsPane, 400,400);
+		settingsScene = new Scene(settingsPane, 400, 400);
+		predictScene = new Scene(predictPane, 400, 400);
 		mainStage.setScene(homeScene);
 		mainStage.show();
 	}
@@ -426,6 +537,42 @@ public class AppMain extends Application {
 		toShow.show();
 	}
 	
+	private void handlePredict(Label predictMessage, Label predictedValue, Button redo) {
+		
+		try {
+			
+			String toCheck;
+			
+			toCheck = (String)in.readObject();
+			
+			
+			if(toCheck.equals("QUERY")) {
+				
+				predictMessage.setText((String)in.readObject());
+			} else {
+				
+				predictedValue.setText(((Double)in.readObject()).toString());
+				//Alert confirmation = new Alert(AlertType.CONFIRMATION);
+				//confirmation.setContentText("Vuoi ripetere?");
+				//confirmation.show();
+				
+				predictMessage.setText("Vuoi ricominciare?");
+				redo.setDisable(false);
+				
+					
+				//out.writeObject(-1);
+				
+			}
+				
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * Metodo utilizzato per tenere aggiornati le stringhe di prompt nei campi
